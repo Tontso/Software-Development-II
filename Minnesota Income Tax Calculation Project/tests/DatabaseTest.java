a76ea7dcaea0bf081810cf6e6877244380356670
Tontso <tommysketo977@gmail.com>
2020-11-13 09:42:31 +0200

    Change to junit5

---
+++ Minnesota Income Tax Calculation Project/tests/DatabaseTest.java
@@ -0,0 +1,51 @@
+package tests;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+
+class DatabaseTest {
+
+	@Test
+	@Order(1)
+	void testProccessTaxpayersDataFromFilesIntoDatabase() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testAddTaxpayerToList() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetTaxpayersArrayListSize() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetTaxpayerFromArrayList() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testRemoveTaxpayerFromArrayList() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetTaxpayerNameAfmValuesPairList() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetTaxpayersNameAfmValuesPairList() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testUpdateTaxpayerInputFile() {
+		fail("Not yet implemented");
+	}
+
+}
